c44176ca5f600eba02285dff2205a489c203d112
leonardo <ferrari2204_5@hotmail.com>
2018-01-13 00:47:04 -0200

    Initial commit.

---
+++ android/app/src/main/java/com/bloquinhossp2018/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bloquinhossp2018;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "BloquinhosSp2018";
+    }
+}
