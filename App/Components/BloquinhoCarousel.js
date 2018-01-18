d90b584a71937883e979453fc5937fe2fef53e32
leonardo <ferrari2204_5@hotmail.com>
2018-01-18 00:54:33 -0200

    fix text overflow, add styles

--- App/Components/BloquinhoCarousel.js
+++ App/Components/BloquinhoCarousel.js
@@ -2,62 +2,26 @@ import React, { Component } from 'react'
 import { Dimensions, View, Text, Image } from 'react-native'
 import Carousel from 'react-native-snap-carousel'
 
-const horizontalMargin = 40
-const sliderWidth = Dimensions.get('window').width
-const itemWidth = sliderWidth - 2 * horizontalMargin
+import { styles, sliderWidth, itemWidth } from './Styles/BloquinhosCarouselStyles'
 
 export default class BloquinhoCarousel extends Component {
 
     _renderItem ({item, index}) {
         return (
-            <View style={{ flex: 1,
-              width: itemWidth,
-              backgroundColor: item.backgroundColor,
-              height: 99,
-              borderRadius: 6,
-              shadowColor: "rgba(0, 0, 0, 0.13)",
-              shadowOffset: {
-                width: 0,
-                height: 2
-              },
-              shadowRadius: 0,
-              shadowOpacity: 1,
-              borderStyle: "solid",
-              borderWidth: 1,
-              borderColor: "rgba(151, 151, 151, 0.2)",
-              flexDirection: 'row',
-            }}>
+            <View style={styles.container}>
               <Image
                 source={{uri: 'https://instagram.fgru5-1.fna.fbcdn.net/vp/a807d5a6d52823dc0614e1cdd92d32bb/5ADF0E92/t51.2885-15/s750x750/sh0.08/e35/14488217_151215075346617_1886875705472450560_n.jpg'}}
-                style={{
-                  marginTop: 10,
-                  marginLeft: 10,
-                  width: 75,
-                  height: 75,
-                  borderRadius: 37.5
-                }}
-              >
+                style={styles.image}>
               </Image>
-              <View>
+              <View style={styles.textContainer}>
                 <Text
-                  style={{
-                    marginTop: 10,
-                    marginLeft: 10,
-                    fontFamily: "Avenir",
-                    fontSize: 20,
-                    fontWeight: "500",
-                  }}
-                >{item.title}
+                  style={styles.title}>
+                  {item.title}
                 </Text>
                 <Text
-                  style={{
-                    marginTop: 10,
-                    marginLeft: 10,
-                    fontFamily: "Avenir",
-                    fontSize: 15,
-                    fontWeight: "500",
-                  }}
-                >Sei que lá sei que lá sei que lá sei que lá sei que lá sei que lá sei que lá </Text>
+                  style={styles.subtitle}>
+                  Sei que lá sei que lá sei que lá sei que lá sei que lá sei que lá sei que lá 
+                  </Text>
               </View>
             </View>
         );
---
+++ App/Components/Styles/BloquinhosCarouselStyles.js
@@ -0,0 +1,54 @@
+import {
+  StyleSheet,
+  Dimensions
+} from 'react-native'
+
+const horizontalMargin = 40
+export const sliderWidth = Dimensions.get('window').width
+export const itemWidth = sliderWidth - 2 * horizontalMargin
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: itemWidth,
+    backgroundColor: 'red',
+    height: 99,
+    borderRadius: 6,
+    shadowColor: "rgba(0, 0, 0, 0.13)",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 0,
+    shadowOpacity: 1,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "rgba(151, 151, 151, 0.2)",
+    flexDirection: 'row',
+  },
+  image: {
+    marginLeft: 10,
+    width: 75,
+    height: 75,
+    alignSelf: 'center',
+    borderRadius: 37.5
+  },
+  textContainer: {
+    flexGrow: 1,
+    width: 0
+  },
+  title: {
+    marginTop: 10,
+    marginLeft: 10,
+    fontFamily: "Avenir",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  subtitle: {
+    marginTop: 10,
+    marginLeft: 10,
+    fontFamily: "Avenir",
+    fontSize: 15,
+    fontWeight: "500",
+  }
+})
