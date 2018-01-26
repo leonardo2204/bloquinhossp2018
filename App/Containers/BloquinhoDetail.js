5c23e548bd54c11092524f3aab0ed3a5e16e4d92
leonardo <ferrari2204_5@hotmail.com>
2018-01-26 11:40:16 -0200

    initial commit

--- App/Containers/BloquinhoDetail.js
+++ App/Containers/BloquinhoDetail.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, Text, Image, View } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
 // Styles
 import styles from './Styles/BloquinhoDetailStyle'
+import { Divider, Icon } from 'react-native-elements';
 
 class BloquinhoDetail extends Component {
   constructor (props) {
     super(props)
-    console.tron.log(this.props.navigation.state.params)
+    
+    this.state = {
+      ...this.props.navigation.state.params.bloquinho
+    }
   }
 
   render () {
     return (
       <ScrollView style={styles.container}>
-        <Text>BloquinhoDetail Container</Text>
+        <Image source={{uri : this.state.picture}} style={{height: 220}} resizeMode='stretch'/>
+        <Text style={{fontSize: 20, padding: 20, textAlign:'center'}}>{this.state.bloco_name}</Text>
+        <Divider style={{height: 2, backgroundColor:'black'}}/>
+        <View style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+          <Icon iconStyle={{padding: 10}} name='schedule'/>
+          <Text>adlasdlasdlasldasldasl</Text> 
+        </View>
+        <View style={{flex:1, flexDirection:'row', alignItems:'center'}}>
+          <Icon iconStyle={{padding: 10}} name='location-on'/>
+          <Text>adlasdlasdlasldasldasl</Text> 
+        </View>
       </ScrollView>
     )
   }
--- App/Containers/Styles/BloquinhoDetailStyle.js
+++ App/Containers/Styles/BloquinhoDetailStyle.js
@@ -1,10 +1,6 @@
 import { StyleSheet } from 'react-native'
-import { Colors, Metrics } from '../../Themes/'
+import { ApplicationStyles, Colors, Metrics } from '../../Themes/'
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Metrics.navBarHeight,
-    backgroundColor: Colors.background
-  }
+  ...ApplicationStyles.screen,
 })
